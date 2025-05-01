9d9cf6054aef773d154b380010320c37d498220a
Shafran Ashraff <shafran@kayatech.com>
2025-05-01 16:51:06 +0530

    Implement chatbot API endpoint and request model; update LLM model to gpt-4o-mini

--- app/app.py
+++ app/app.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 from langchain_core.output_parsers import StrOutputParser
+from langchain_core.messages import HumanMessage, AIMessage
 from dotenv import load_dotenv
 from models.llm import get_llm
+from utils.prompt import get_prompt
+from entity.chat_request import ChatRequest
 from logger import logging
 import os
 
@@ -31,3 +34,40 @@ async def index():
         "response": "Welcome to Chatbot"
     }
     return response_data
+
+@app.post("/api/chatbot")
+async def chatbot(req: ChatRequest):
+    global history
+
+    question = req.question
+    logging.info(f"Question: {question}")
+
+    response_data = {
+        "status": "success",
+        "response": ""
+    }
+
+    try:
+        prompt = get_prompt()
+        logging.info("Received Prompt Template")
+
+        chain = prompt | llm | parser
+        logging.info("Created chain")
+
+        history = history[-4:]
+
+        response = chain.invoke({"history": history, "question": [
+            HumanMessage(content=question)
+        ]})
+        logging.info(f"Response: {response}")
+
+        history.extend([HumanMessage(content=question),
+                        AIMessage(content=response)])
+        
+        response_data["response"] = response
+        return response_data
+    except Exception as e:
+        logging.error(f"Error: {str(e)}")
+        response_data["response"] = str(e)
+        response_data["status"] = "fail"
+        return response_data
--- app/models/llm.py
+++ app/models/llm.py
@@ -8,7 +8,7 @@ def get_llm():
     os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 
     llm = ChatOpenAI(
-        model="gpt-3.5-turbo",
+        model="gpt-4o-mini",
         temperature=0
     )
 
